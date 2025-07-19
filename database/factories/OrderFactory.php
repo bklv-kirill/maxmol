7abfc74c6ca2db4622b9b700c62c48ab1cb13295
bklv-kirill <bklv.kirill@yandex.ru>
2025-07-19 23:15:02 +0300

    Modify Order factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Order;
+use App\Models\Warehouse;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class OrderFactory extends Factory
@@ -12,6 +13,13 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
+            'warehouse_id' => Warehouse::query()->inRandomOrder()->first()->id,
+            'customer' => fake()->name(),
+            'status' => $this->faker->randomElement([
+                Order::STATUS_ACTIVE,
+                Order::STATUS_COMPLETED,
+                Order::STATUS_CANCELLED,
+            ])
         ];
     }
 }
