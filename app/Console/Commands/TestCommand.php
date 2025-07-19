7451c459d149f68d164136dfab6130eb687d6284
bklv-kirill <bklv.kirill@yandex.ru>
2025-07-19 22:42:36 +0300

    Add command for running tests

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    protected $signature = 'test:run';
+
+    protected $description = 'Command for running test';
+
+    public function handle(): void
+    {
+        dd(fake()->sentence(3));
+        dd('test');
+    }
+}
