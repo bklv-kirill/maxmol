207cc69ae6ddb7620b769a40a9ba491a30e4a087
bklv-kirill <bklv.kirill@yandex.ru>
2025-07-19 22:41:30 +0300

    Modify database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\Stock;
 use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
 
@@ -17,5 +18,23 @@ class DatabaseSeeder extends Seeder
         Warehouse::factory()
             ->count(10)
             ->create();
+
+        $warehouses = Warehouse::query()
+            ->get();
+
+        foreach ($warehouses as $warehouse) {
+            $products = Product::query()
+                ->inRandomOrder()
+                ->limit(10)
+                ->get();
+
+            foreach ($products as $product) {
+                Stock::query()->create([
+                    'product_id' => $product->id,
+                    'warehouse_id' => $warehouse->id,
+                    'stock' => rand(10, 100),
+                ]);
+            }
+        }
     }
 }
